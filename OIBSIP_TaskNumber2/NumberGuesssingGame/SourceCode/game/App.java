a6304aed96847a49619d92c9d11efdbb199cf081
Akpraj1932 <109778513+Akpraj1932@users.noreply.github.com>
2022-07-26 11:21:25 +0530

    Update App.java

--- OIBSIP_TaskNumber2/NumberGuesssingGame/SourceCode/game/App.java
+++ OIBSIP_TaskNumber2/NumberGuesssingGame/SourceCode/game/App.java
@@ -1,86 +1,44 @@
-package game;
+// Java program for the above approach
 import java.util.Scanner;
-import java.util.Random;
-public class App {
-    public int randomNumber;
-    public int Guesses;
-    public int userInput;
-    public int attempts = 10;
 
-
-    App() {
-        Random r = new Random();
-        this.randomNumber = r.nextInt(100);
-    }
-
-    public boolean isCorrectNumber() {
-        Guesses++;
-        attempts--;
-        if (userInput == randomNumber) {
-            System.out.println("\t* YOUR GUESS IS CORRECT! \n\t* NO OF GUESSES=>"+Guesses);
-            System.out.println();
-            return true;
-        } else if (userInput > 100) {
-            System.out.println("\t* Your Number is out of Range..Enter a Number between [1-100]");
-            System.out.println("\t* Number of attempts left=>" + attempts);
-            System.out.println();
-
-        } else if (userInput < randomNumber) {
-            System.out.println("\t* [Number is less than the Random Number]");
-            System.out.println("\t* Number of attempts left=>" + attempts);
-            System.out.println();
-        } else if (userInput > randomNumber) {
-            System.out.println("\t* [Number is Greater than the Random Number]");
-            System.out.println("\t* Number of attempts left=>" + attempts);
-            System.out.println();
-        }
-
-        return false;
-    }
-
-    public void UserInput() {
-        try {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("* Guess the Number [1-100] =>");
-            userInput = sc.nextInt();
-        } catch (Exception e) {
-            System.out.println("\t* ENTER A WHOLE NUMBER");
-
-        }
-    }
-
-    public static void main(String[] args) {
-        boolean flag = true;
+public class GFG {
+    public static void guessingNumberGame()
+    {
         Scanner sc = new Scanner(System.in);
-        while (flag != false) {
-            System.out.println("**********NUMBER GUESSING GAME**********");
-            System.out.println();
-            System.out.println("********YOU ONLY HAVE 10 ATTEMPTS*******");
-            System.out.println();
-            App a = new App();
-            boolean res = false;
-            while (res != true) {
-                if (a.attempts <= 0) {
-                    break;
-                }
-                a.UserInput();
-                res = a.isCorrectNumber();
+        int number = 1 + (int)(100 * Math.random());
+        int K = 5;
+
+        int i, guess;
+
+        System.out.println(
+                "A number is chosen" + " between 1 to 100." + "Guess the number" + " within 5 trials.");
+
+        for (i = 0; i < K; i++) {
+
+            System.out.println("Guess the number:");
+
+            guess = sc.nextInt();
+            if (number == guess) {
+                System.out.println("Congratulations!" + " You guessed the number.");
+                break;
             }
-            if (a.attempts == 0) {
-                System.out.println("\tXXXXXXX YOU ARE OUT OF LIVES :( XXXXXXX");
-                System.out.println();
-                System.out.println();
+            else if (number > guess && i != K - 1) {
+                System.out.println("The number is " + "greater than " + guess);
             }
-            System.out.println("******DO YOU WISH TO CONTINUE THE GAME******");
-            System.out.println("\t* Press Y to Continue\n\t* Press N to Stop");
-            String ch = sc.next();
-            if(ch.equals("Y"))flag=true;
-            else if (ch.equals("N")) {
-            flag=false;
-                System.out.println("***** GAME OVER *****");
-            break;
+            else if (number < guess && i != K - 1) {
+                System.out.println("The number is" + " less than " + guess);
             }
-            System.out.println();
         }
+
+        if (i == K) {
+            System.out.println("You have exhausted" + " K trials.");
+
+            System.out.println("The number was " + number);
+        }
+    }
+    public static void
+    main(String arg[])
+    {
+        guessingNumberGame();
     }
 }
